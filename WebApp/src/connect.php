b04a26f5c41015d3547735ea6ae0060e04b9edc7
silasade <100533433+silasade@users.noreply.github.com>
2023-05-10 11:25:59 +0100

    Update connect.php

--- WebApp/src/connect.php
+++ WebApp/src/connect.php
@@ -6,9 +6,8 @@
     $gender=$_POST['gender'];
     $zip=$_POST['zip'];
     $about=$_POST['about'];
-    
-    if($name=="" || $email=="" || $email==""|| $password==""|| $number==""|| $gender=="" || $about=="" || $zip=="" ){
-        
+    $language=$_POST['languages'];
+    if($name=="" || $email=="" || $email==""|| $password==""|| $number==""|| $gender=="" || $about=="" || $zip=="" || $language==""){
         header("Refresh: 3; URL=http://localhost/WebApp/src/index.php");
         echo "Please make sure you fill each box";
         
@@ -18,8 +17,8 @@
     else{
         $con=mysqli_connect('localhost', 'root');
         mysqli_select_db($con,'signin') or die('Failure to communicate with database: '.mysqli_connect_error());
-        $query =  "INSERT INTO login (name,email,password,number,gender,zip,about)
-            VALUES('$name', '$email', '$password', '$number', '$gender','$zip','$about')";
+        $query =  "INSERT INTO login (name,email,password,number,gender,zip,about,languages)
+            VALUES('$name', '$email', '$password', '$number', '$gender','$zip','$about','$language')";
         $result = mysqli_query($con,$query);
         usleep(10);
         echo "Successful";
